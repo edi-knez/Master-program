9bddcf0ba619b8a35152a1b7a45b795c15d42594
edi-knez <53223491+edi-knez@users.noreply.github.com>
2024-02-10 21:09:33 +0100

    refactor setup.cpp datoteke; premjestene deklaracije funkcija u zasebnu header datoteku da ne smetaju

--- Master program/setup.cpp
+++ Master program/setup.cpp
@@ -6,6 +6,7 @@
 /// koristi namespace funkcije za projekte
 /// koristenje MACRO za dodavanje novih funkcija na popis
 /// dodavanje klasa
+#include "PotrebneDatotekeIDeklaracijeFunkcija.h"
 
 #include <iostream>
 #include <array>
@@ -36,100 +37,6 @@ struct Zadatak
 	const char* kod; // tijelo funkcije
 };
 
-//////////////////////////////////////
-/// ////////////////////////////////// potrebne datoteke iz projekta C++ knjiga
-//////////////////////////////////////
-#include "../_Projekti/C++ knjiga/Cjelina1.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina2.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina3.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina4.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina5.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina6.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina7.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina8.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina9.hpp"
-#include "../_Projekti/C++ knjiga/Cjelina10.hpp"
-//////////////////////////////////////
-
-//////////////////////////////////////
-/// ////////////////////////////////// potrebne funkcije iz projekta FESB
-//////////////////////////////////////
-extern void fesb_vj1_1();	extern void fesb_vj1_2();	extern void fesb_vj1_3();	extern void fesb_vj1_4();
-extern void fesb_vj2_1();	extern void fesb_vj2_2();	extern void fesb_vj2_3();	extern void fesb_vj2_4();	extern void fesb_vj2_5();
-extern void fesb_vj3_1();	extern void fesb_vj3_2();	extern void fesb_vj3_3();	extern void fesb_vj3_4();
-extern void fesb_vj4_1();	extern void fesb_vj4_2();	extern void fesb_vj4_3();
-extern void fesb_vj5_1();	extern void fesb_vj5_2();	extern void fesb_vj5_3();	extern void fesb_vj5_4();
-extern void fesb_vj6_1();	extern void fesb_vj6_2();	extern void fesb_vj6_3();	extern void fesb_vj6_4();
-extern void fesb_vj7_1();	extern void fesb_vj7_2();	extern void fesb_vj7_3();	extern void fesb_vj7_4();
-extern void fesb_vj8_1();	extern void fesb_vj8_2();	extern void fesb_vj8_3();	extern void fesb_vj8_4();	extern void fesb_vj8_5();	extern void fesb_vj8_6();
-extern void fesb_vj9_1();	extern void fesb_vj9_2();	extern void fesb_vj9_3();
-extern void fesb_vj10_1();	extern void fesb_vj10_2();	extern void fesb_vj10_3();	extern void fesb_vj10_4();
-extern void fesb_vj11_1();	extern void fesb_vj11_2();	extern void fesb_vj11_3();	extern void fesb_vj11_4();
-extern void fesb_vj12_1();	extern void fesb_vj12_2();	extern void fesb_vj12_3();	extern void fesb_vj12_4();
-//////////////////////////////////////
-
-
-//////////////////////////////////////
-/// ////////////////////////////////// potrebne funkcije iz projekta razno
-//////////////////////////////////////
-extern void Practical_Cpp_Programming_pog10_1(); extern void Practical_Cpp_Programming_pog10_2(); extern void Practical_Cpp_Programming_pog10_3();
-extern void Practical_Cpp_Programming_pog10_4();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog11_1(); extern void Practical_Cpp_Programming_pog11_2(); extern void Practical_Cpp_Programming_pog11_3();
-extern void Practical_Cpp_Programming_pog11_4(); extern void Practical_Cpp_Programming_pog11_5(); extern void Practical_Cpp_Programming_pog11_6();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog13_1(); extern void Practical_Cpp_Programming_pog13_2(); extern void Practical_Cpp_Programming_pog13_3();
-extern void Practical_Cpp_Programming_pog13_4(); extern void Practical_Cpp_Programming_pog13_5();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog13_1(); extern void Practical_Cpp_Programming_pog13_2(); extern void Practical_Cpp_Programming_pog13_3();
-extern void Practical_Cpp_Programming_pog13_4();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog14_1(); extern void Practical_Cpp_Programming_pog14_2(); extern void Practical_Cpp_Programming_pog14_3();
-extern void Practical_Cpp_Programming_pog14_4();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog15_1(); extern void Practical_Cpp_Programming_pog15_2();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog16_1(); extern void Practical_Cpp_Programming_pog16_2(); extern void Practical_Cpp_Programming_pog16_3();
-extern void Practical_Cpp_Programming_pog16_4(); extern void Practical_Cpp_Programming_pog16_5(); extern void Practical_Cpp_Programming_pog16_6();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog17_2__1(); extern void Practical_Cpp_Programming_pog17_2__2(); extern void Practical_Cpp_Programming_pog17_2__3();
-extern void Practical_Cpp_Programming_pog17_5();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog18_1(); extern void Practical_Cpp_Programming_pog18_2(); extern void Practical_Cpp_Programming_pog18_4();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog19_1();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog21_1(); extern void Practical_Cpp_Programming_pog21_2(); extern void Practical_Cpp_Programming_pog21_3();
-extern void Practical_Cpp_Programming_pog21_5();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog22_4();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog23_1(); extern void Practical_Cpp_Programming_pog23_2(); extern void Practical_Cpp_Programming_pog23_3();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog24_1();
-/////////////////////////////////////
-extern void Practical_Cpp_Programming_pog26_1(); extern void Practical_Cpp_Programming_pog26_3(); extern void Practical_Cpp_Programming_pog26_5();
-/////////////////////////////////////
-
-//////////////////////////////////////
-/// //////////////////////////////////
-//////////////////////////////////////
-/// poglavlje 2:
-extern void ThinkLAP_printSidewaysTriangle(); extern void ThinkLAP_luhnFormulaPrecomputed_and_Expression(); extern void ThinkLAP_decodeMessage();
-extern void ThinkLAP_pog2vj1(); extern void ThinkLAP_pog2vj2(); extern void ThinkLAP_pog2vj2version2(); extern void ThinkLAP_pog2vj3(); extern void ThinkLAP_pog2vj5_check(); extern void ThinkLAP_pog2vj5_generate(); extern void ThinkLAP_pog2vj6(); extern void ThinkLAP_pog2vj6_2(); extern void ThinkLAP_pog2vj7(); extern void ThinkLAP_pog2vj8(); extern void ThinkLAP_pog2vj9();
-
-/// poglavlje 3:
-extern void ThinkLAP_pog3_finding_the_mode(); extern void ThinkLAP_pog3_vj1_sorting(); void ThinkLAP_pog3_highestSales_zadano(); extern void ThinkLAP_pog3_vj2_median(); extern void ThinkLAP_pog3_vj3_issorted(); extern void ThinkLAP_pog3_vj4_encode_and_vj5_decode(); extern void ThinkLAP_pog3_vj8_grade_better_than(); extern void ThinkLAP_pog3_vj9_modified_median();
-
-/// poglavlje 4:
-extern void ThinkLAP_pog4_osnovne_operacije_stringa(); extern void ThinkLAP_pog4_prosjek_ocjena_studenata(); extern void ThinkLAP_pog4_vj1(); extern void ThinkLAP_pog4_vj2_jos_operacija_nad_stringom(); extern void ThinkLAP_pog4_myCharPtr_klasa_testiranje(); extern void ThinkLAP_pog4_vj4_dodavanje_and_vj5_uklananje_studentkih_rekorda(); extern void ThinkLAP_pog4_vj6(); extern void ThinkLAP_pog4_vj7(); extern void ThinkLAP_pog4_vj8(); extern void ThinkLAP_pog4_vj9(); extern void ThinkLAP_pog4_vj10();
-
-/// poglavlje 5:
-extern void ThinkLAP_pog5_vj1(); extern void ThinkLAP_pog5_zad1(); extern void ThinkLAP_pog5_zad2(); extern void ThinkLAP_pog5_zad3_i_4(); extern void ThinkLAP_pog5_zad5();
-
-/// poglavlje 6:
-extern void ThinkLAP_pog6_vj1(); extern void ThinkLAP_pog6_vj2(); extern void ThinkLAP_pog6_vj3(); extern void ThinkLAP_pog6_vj4(); extern void ThinkLAP_pog6_vj5(); extern void ThinkLAP_pog6_zad1(); extern void ThinkLAP_pog6_zad2(); extern void ThinkLAP_pog6_zad3(); extern void ThinkLAP_pog6_zad4(); extern void ThinkLAP_pog6_zad5(); extern void ThinkLAP_pog6_zad6();
-//////////////////////////////////////
 
 
 //////////////////////////////////////
@@ -173,6 +80,8 @@ void popuniCijeliPopisFunkcija()
 		popuniPopisFunkcijaZa( cur );
 	}
 }
+
+
 // ZA KASNIJE
 std::pair<std::string, std::string> getName()
 {
